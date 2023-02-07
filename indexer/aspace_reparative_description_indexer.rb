9b17769b166459d01b209f769ceadbb3dff0d388
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2023-02-07 15:28:33 -0500

    fix indexer and facet locale

--- indexer/aspace_reparative_description_indexer.rb
+++ indexer/aspace_reparative_description_indexer.rb
@@ -1,7 +1,7 @@
 class IndexerCommon
 
   add_indexer_initialize_hook do |indexer|
-    if AppConfig[:plugins].include?('aspace_reparative_description')
+    if AppConfig[:plugins].include?('aspace_reparative_descriptions')
       indexer.add_document_prepare_hook {|doc, record|
         if ['accession','resource', 'archival_object', 'digital_object', 'digital_object_component'].include?(doc['primary_type']) && record['record']['reparative_descriptions']
           doc['reparative_descriptions_u_sbool'] = true
