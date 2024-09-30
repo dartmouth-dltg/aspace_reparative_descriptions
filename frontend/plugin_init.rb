2a2aaa39e49c6bfbc87d7df41cd2633ea3e89a0d
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2024-09-30 13:26:01 -0400

    changes for AS 4.0

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -3,12 +3,7 @@ require_relative '../lib/reparative_descriptions_helper'
 ArchivesSpace::Application.config.after_initialize do
   # only add faceting if configured
   if AppConfig.has_key?(:aspace_reparative_descriptions_faceting) && AppConfig[:aspace_reparative_descriptions_faceting] == true
-    SearchResultData.class_eval do
-      self.singleton_class.send(:alias_method, :BASE_FACETS_pre_reparative_descriptions, :BASE_FACETS)
-      def self.BASE_FACETS
-        self.BASE_FACETS_pre_reparative_descriptions << "reparative_descriptions_u_sbool"
-      end
-    end
+    Plugins::add_search_base_facets('reparative_descriptions_u_sbool')
   end
 
   JSONModel(:reparative_description)
